e9cdfb3b803c6b2f6a5e7c265952f6b8345bb9e8
Seema Thapa <tseema@insightworkshop.io>
2020-06-24 10:01:27 +0545

    [Add] added horizontal scrolling

--- myapp/src/app/practice/infinite-scrolling/infinite-scrolling.component.ts
+++ myapp/src/app/practice/infinite-scrolling/infinite-scrolling.component.ts
@@ -1,9 +1,10 @@
-import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-infinite-scrolling',
   templateUrl: './infinite-scrolling.component.html',
   styleUrls: ['./infinite-scrolling.component.css'],
+  encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InfiniteScrollingComponent implements OnInit {
